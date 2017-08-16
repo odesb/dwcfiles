ae8c6fe4aa610b82db3c52767c7c1f127e6c0e64
Olivier Desbiens <desbiens.olivier@gmail.com>
2017-08-16 17:05:05 -0400

    Added GridFS storage, made thumbnail generation required for html5 content, cleaned up code

--- static/js/dietwater.js
+++ static/js/dietwater.js
@@ -1,9 +1,9 @@
-$(function() {
+(function() {
 	var multimediaButton = $('button.multimedia');
 	var media_next = 1;
 	multimediaButton.on('click', function() {
 		multimediaButton.toggleClass('is-loading');
-		$.get($SCRIPT_ROOT + '/_ajax_multimedia?next=' + media_next, function(data) {
+		$.get($SCRIPT_ROOT + '/_ajax_more?html5=true&next=' + media_next, function(data) {
 			multimediaButton.parent().before(data);
 			media_next += 1;
 		});
@@ -13,13 +13,13 @@ $(function() {
 	var other_next = 1;
 	otherButton.on('click', function() {
 		otherButton.toggleClass('is-loading');
-		$.get($SCRIPT_ROOT + '/_ajax_other?next=' + other_next, function(data) {
+		$.get($SCRIPT_ROOT + '/_ajax_more?html5=&next=' + other_next, function(data) {
 			otherButton.parent().before(data);
 			other_next += 1;
 		});
 		otherButton.toggleClass('is-loading');
 	});
-});
+})();
 var d = document.getElementById('submit');
 function loadButton() {
 	d.classList.add('is-loading');
