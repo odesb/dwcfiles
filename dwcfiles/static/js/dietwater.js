d98f1afe695b24627121665035c52f29d9bb563f
xisbroken <desbiens.olivier@gmail.com>
2018-01-26 00:52:18 -0500

    Purged jQuery

--- dwcfiles/static/js/dietwater.js
+++ dwcfiles/static/js/dietwater.js
@@ -38,32 +38,3 @@ function extractFilenameEvent() {
   });
 }
 
-
-/*
-(function() {
-  var multimediaButton = $('button.multimedia');
-  var media_next = 1;
-  multimediaButton.on('click', function() {
-    multimediaButton.toggleClass('is-loading');
-    $.get($SCRIPT_ROOT + '/_ajax_more?html5=true&next=' + media_next, function(data) {
-      multimediaButton.parent().before(data);
-      media_next += 1;
-    });
-    multimediaButton.toggleClass('is-loading');
-  });
-  var otherButton = $('button.other');
-  var other_next = 1;
-  otherButton.on('click', function() {
-    otherButton.toggleClass('is-loading');
-    $.get($SCRIPT_ROOT + '/_ajax_more?html5=&next=' + other_next, function(data) {
-      otherButton.parent().before(data);
-      other_next += 1;
-    });
-    otherButton.toggleClass('is-loading');
-  });
-})();
-var d = document.getElementById('submit');
-function loadButton() {
-  d.classList.add('is-loading');
-}
-*/
